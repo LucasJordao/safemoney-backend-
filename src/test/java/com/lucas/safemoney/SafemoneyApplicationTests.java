e92882cf0bb6d81291f77ef396a55554f37e38a1
LucasJordao <lucaswill12@hotmail.com>
2021-01-28 14:06:17 -0300

    Iniciando projeto e instalando dependências necessárias

---
+++ src/test/java/com/lucas/safemoney/SafemoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.safemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafemoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
